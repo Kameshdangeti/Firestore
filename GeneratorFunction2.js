c89b017790201e9e0f9bc1b16d41713ffc30ed76
kameshdangeti <kameshdangeti@gmail.com>
2024-02-06 15:56:05 +0530

    generator function program has been commited

---
+++ GeneratorFunction2.js
@@ -0,0 +1,43 @@
+//Calling generator function from another function
+function* f1() 
+{ 
+	yield 2; 
+	yield 3; 
+} 
+function* f2() 
+{ 
+	yield 1; 
+	yield* f1(); 
+	yield 4; 
+} 
+for (let res of f2()) 
+{ 
+	console.log(res); 
+}
+
+
+//Using Async generator
+const p1 = () => { 
+	return new Promise((resolve, reject) => { 
+		setTimeout(() => resolve(5), 5000) 
+	}) 
+} 
+const P2 = () => { 
+	return new Promise((resolve, reject) => { 
+		setTimeout(() => resolve(6), 3000) 
+	}) 
+} 
+async function* generator() 
+{ 
+	const res1 = await p1(); 
+	yield res1; 
+	const res2 = await P2(); 
+	yield res2; 
+} 
+let i = generator(); 
+for (let v of i) 
+{ 
+	console.log(v); 
+}
+
+
