a22e0fb7da814090ca6d4daf39d378188c888913
kameshdangeti <kameshdangeti@gmail.com>
2024-02-06 13:02:27 +0530

    many promises are implented in this method and commited

---
+++ promises2.js
@@ -0,0 +1,20 @@
+function asynchmethod() {
+	let firstpromise = 
+		new Promise((resolve, reject) => resolve("Hello"));
+	let secondpromise = 
+		new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(" How Are You..");
+		}, 1000);
+	});
+	let bothpromise = 
+		Promise.all([firstpromise, secondpromise]);
+	return bothpromise;
+}
+
+async function display() {
+	let d = await asynchmethod();
+	console.log(d);
+}
+
+display();
