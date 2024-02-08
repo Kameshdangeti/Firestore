e37ec4fff90ca5935a07d907e040be6cb6e772ce
kameshdangeti <kameshdangeti@gmail.com>
2024-02-08 11:44:04 +0530

    Functions using async generator has been modified and commited

--- PromisesFunctions/Function_Using_Async_generator.js
+++ PromisesFunctions/Function_Using_Async_generator.js
@@ -1,23 +1,36 @@
 //Using Async generator
-const p1 = () => { 
-	return new Promise((resolve, reject) => { 
-		setTimeout(() => resolve(5), 5000) 
-	}) 
-} 
-const P2 = () => { 
-	return new Promise((resolve, reject) => { 
-		setTimeout(() => resolve(6), 3000) 
-	}) 
-} 
-async function* myGenerator() 
-{ 
-	const res1 = await p1(); 
-	yield res1; 
-	const res2 = await P2(); 
-	yield res2; 
-} 
-let i = myGenerator(); 
-for await (let v of myGenerator()) 
-{ 
-	console.log(v); 
+const p1 = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => resolve(5), 5000)
+	})
 }
+const P2 = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => resolve(6), 3000)
+	})
+}
+async function* myGenerator() {
+	const res1 = await p1();
+	yield res1;
+	const res2 = await P2();
+	yield res2;
+}
+let i = myGenerator();
+
+async function test(){
+	for await (let v of i) {
+		console.log(v);
+	}
+};
+
+// Anonymous Execution
+
+
+// (async ()=> {
+// 	for await (let v of i) {
+// 		console.log(v);
+// 	}
+// })();
+
+// Named Function Execution
+test();
