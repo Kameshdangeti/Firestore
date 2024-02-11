0f64db043b240952809fcb2cb002c3d4b6245e3b
kameshdangeti <kameshdangeti@gmail.com>
2024-02-11 15:39:05 +0530

    Using  settimeout in async functio

--- PromisesFunctions/Async-Await_without_reject.js
+++
@@ -1,9 +0,0 @@
-async function myDisplay() {
-    let myPromise = new Promise(function(resolve) //Error occurs due to absence of reject.
-    {
-      resolve("Good Morning");
-    });
-    document.getElementById("demo").innerHTML = await myPromise;
-  }
-  
-  myDisplay();
