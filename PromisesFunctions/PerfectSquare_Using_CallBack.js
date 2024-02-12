d1106418383a920a81a307ccc28f7322b9f75225
kameshdangeti <kameshdangeti@gmail.com>
2024-02-12 16:41:39 +0530

    perfect square using callback function has been commited

--- PromisesFunctions/PerfectSquare_Using_CallBack.js
+++ PromisesFunctions/PerfectSquare_Using_CallBack.js
@@ -1,11 +1,11 @@
 function perfectSquare (number, callback) {
-    const bool = Number.isInteger(Math.sqrt(number))
-    if (!bool) {
+    const b = Number.isInteger(Math.sqrt(number))
+    if (!b) {
       return callback(`Number ${number} is NOT a perfect square` )
     }
     callback(`Number ${number} is a perfect square`)
   }
-  
+
   function callback(data){
     console.log(data)
   }
