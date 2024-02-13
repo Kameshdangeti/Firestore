609633ef0575fec8dc1602155cb99669ba11b39c
kameshdangeti <kameshdangeti@gmail.com>
2024-02-13 17:03:18 +0530

    promise function using any  has been commited

---
+++ PromisesFunctions/promise-functions-using-any().js
@@ -0,0 +1,15 @@
+const pErr = new Promise((resolve, reject) => {
+    reject("Always fails");
+  });
+  
+  const pSlow = new Promise((resolve, reject) => {
+    setTimeout(resolve, 500, "Done eventually");
+  });
+  
+  const pFast = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, "Done quick");
+  });
+  
+  Promise.any([pErr, pSlow, pFast]).then((value) => {
+    console.log(value);
+  })
